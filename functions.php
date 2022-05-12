edc0ef6b943d66235a19fc36fd8d921dd2afa8cd
Haylee Barber <hayleejbarber@outlook.com>
2022-05-12 15:54:28 -0400

    update

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 function enqueue_my_styles_and_scripts() {
 
-  wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . '')
+  wp_enqueue_style('my-styles', get_stylesheet_directory_uri() . 'wpthemestyles.css', array(), '1.0.0', 'all');
 
 
 
